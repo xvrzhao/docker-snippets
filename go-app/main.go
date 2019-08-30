be06f93f795b6cf7c4cc7e4fca5cf69c36bff914
xvrzhao <xvrzhao@gmail.com>
2019-08-30 17:49:10 +0800

    set image timezone to CST; change base image of node-app to Alpine

--- go-app/main.go
+++ go-app/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Welcome to application of Go!")
+		c.String(200, "Welcome to application of Go! %s", time.Now())
 	})
 	r.Run()
 }
--- node-app/index.js
+++ node-app/index.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-    res.send('Welcome to application of Node.js!')
+    res.send('Welcome to application of Node.js! ' + (new Date).toString())
 })
 
 app.listen(3000)
